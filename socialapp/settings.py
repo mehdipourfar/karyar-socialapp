75be428204743eb3c86a18ae760f168866acc2e5
Mehdi Pourfar <mehdipourfar@gmail.com>
2024-06-30 19:43:39 +0330

    add user model

--- socialapp/settings.py
+++ socialapp/settings.py
@@ -37,6 +37,10 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+
+    "django_extensions",
+
+    "user",
 ]
 
 MIDDLEWARE = [
@@ -75,8 +79,9 @@ WSGI_APPLICATION = "socialapp.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.postgresql",
+        "USER": "postgres",
+        "NAME": "socialapp",
     }
 }
 
@@ -121,3 +126,6 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+
+AUTH_USER_MODEL = "user.User"
---
+++ user/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+
+from django.contrib.auth.models import AbstractUser
+from django.utils.translation import gettext_lazy as _
+
+
+class User(AbstractUser):
+    email = models.EmailField(_("email address"), blank=True, null=True)
+
+    REQUIRED_FIELDS = []
