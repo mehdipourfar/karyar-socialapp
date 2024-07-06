3fe939bfd9a6431eee4a4a28063b4b9a13321d79
Mehdi Pourfar <mehdipourfar@gmail.com>
2024-07-06 19:38:43 +0330

    add view for create user in pure django

--- user/views.py
+++ user/views.py
@@ -1,3 +1,27 @@
+import json
+
+from django.http import HttpRequest, HttpResponse
 from django.shortcuts import render
 
-# Create your views here.
+from django.views.decorators.csrf import csrf_exempt
+
+
+from .models import User
+
+
+
+
+@csrf_exempt
+def register(request: HttpRequest) -> HttpResponse:
+    if request.method != 'POST':
+        return HttpResponse(status=405)
+
+    body = json.loads(request.body)
+
+    username = body.get("username")
+    password = body.get("password")
+
+    print(username)
+    print(password)
+
+    return HttpResponse(status=200, content)
